610c155ff553f1232c8bc2cf52de7613711598c8
Roel Christian Yambao <yambao.roelchristian@gmail.com>
2022-11-26 22:32:20 +0800

    Create progress bar

--- src/util/term.py
+++ src/util/term.py
@@ -9,3 +9,10 @@ def clear_screen():
         os.system("cls")
     else:
         os.system("clear")
+
+def progress_bar(current, total, bar_length=20):
+    percent = float(current) * 100 / total
+    arrow = "-" * int(percent / 100 * bar_length - 1) + ">"
+    spaces = " " * (bar_length - len(arrow))
+
+    print("Progress: [%s%s] %d %% (%d / %d)" % (arrow, spaces, percent, current, total), end="\r")
